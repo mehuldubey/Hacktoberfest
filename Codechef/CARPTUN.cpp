c7337ffa799bc5b98a68f7bae522d8b621872589
mehuldubey <44391075+mehuldubey@users.noreply.github.com>
2018-10-23 13:38:40 +0530

    added codechef solutions

--- Codechef/CARPTUN.cpp
+++ Codechef/CARPTUN.cpp
@@ -45,6 +45,79 @@ int main() {
 	return 0;
 } 
 
+//one more solution to the same question 
+#include <bits/stdc++.h>
+#include <climits>
+#define rep(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define srep(i, begin, end) for (__typeof(end) i = begin; i != end; i++)
+#define si(x) int x = scanInt();
+#define sll(x) LL x = scanLong();
+#define pi(x) printf("%d ", x)
+#define pll(x) printf("%lld ", x)
+#define nl printf("\n")
+#define clr(a) memset(a, 0, sizeof(a))
+#define fi first
+#define sec second
+using namespace std;
+typedef unsigned int UI; // 32 bit integer
+typedef long int LI; // 32 bit integer
+typedef unsigned long int ULI; // 32 bit unsigned integer
+typedef long long int LL; // 64 bit integer
+typedef unsigned long long int  ULL; // 64 bit unsigned integer
+typedef long double LD;
+typedef vector<int> VI;
+typedef vector<LL> VLL;
+typedef pair<int, int> PII;
+typedef pair<LL, LL> PLL;
+LL mod = 1e9+7;
+
+/* Fast I/O */
+inline int scanInt() {
+	int n = 0;
+	char ch = getchar_unlocked();
+	int sign = 1;
+	while(ch < '0' || ch > '9') {
+		if(ch == '-')	sign = -1;
+		ch = getchar_unlocked();
+	}
+	while(ch >= '0' && ch <= '9') {
+		n = (n<<1)+(n<<3)+(int)(ch-'0');
+		ch = getchar_unlocked();
+	}
+	return n*sign;
+}
+
+inline LL scanLong() {
+	LL n = 0;
+	char ch = getchar_unlocked();
+	LL sign = 1;
+	while(ch < '0' || ch > '9') {
+		if(ch == '-')	sign = -1;
+		ch = getchar_unlocked();
+	}
+	while(ch >= '0' && ch <= '9') {
+		n = (n<<1)+(n<<3)+(LL)(ch-'0');
+		ch = getchar_unlocked();
+	}
+	return n*sign;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("inp.txt", "r", stdin);
+#endif
+	sll(t);
+	while(t-->0) {
+	
+		sll(n);
+		LL ptime[n];
+		rep(i, 0, n)	ptime[i] = scanLong();
+		sll(c); sll(d); sll(s);
+		cout << fixed << setprecision(15) << (LD)(*max_element(ptime, ptime+n) * (c-1)) << endl;
+	}
+}
+
+
 
 
 
